cf9e07ea3678a3417b4b16ba1b5f49493f2b3caf
Cleyson Lima <cleysonph@gmail.com>
2021-02-16 21:43:51 -0300

    criado o projeto com spring initializr

---
+++ src/main/java/br/com/treinaweb/twclientes/TwclientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.twclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwclientesApplication.class, args);
+	}
+
+}
