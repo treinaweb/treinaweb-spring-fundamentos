87c9fd46e89594999c1931b22c60d2a8ceba48ac
Cleyson Lima <cleysonph@gmail.com>
2021-02-18 00:41:28 -0300

    passando informações do controller para view e da view para o controller

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -3,14 +3,38 @@ package br.com.treinaweb.twclientes.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/mensagem")
+    public ModelAndView mensagem() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Outra mensagem vinda do servidor");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/saudacao")
+    public ModelAndView saudacao(
+        @RequestParam(required = false, defaultValue = "TreinaWeb") String nome
+        ) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+
+        return modelAndView;
     }
     
 }
