afb20b225baec99a60f8340b4b47a0d55a7be019
Cleyson Lima <cleysonph@gmail.com>
2021-02-19 01:21:40 -0300

    action para exibir pagina de listagem de clientes

---
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -0,0 +1,31 @@
+package br.com.treinaweb.twclientes.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.twclientes.model.Cliente;
+import br.com.treinaweb.twclientes.repository.ClienteRepository;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping
+    public ModelAndView listar() {
+        ModelAndView modelAndView = new ModelAndView("cliente/listar.html");
+
+        List<Cliente> clientes = clienteRepository.findAll();
+        modelAndView.addObject("clientes", clientes);
+
+        return modelAndView;
+    }
+    
+}
