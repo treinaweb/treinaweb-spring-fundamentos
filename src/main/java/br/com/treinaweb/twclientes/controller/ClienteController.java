ba3026669e1b550542bb059a95184884e3540402
Cleyson Lima <cleysonph@gmail.com>
2021-02-22 20:29:45 -0300

    action para exibir a página de cadastro

--- src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -44,6 +45,24 @@ public class ClienteController {
         ModelAndView modelAndView = new ModelAndView("redirect:/cliente");
 
         clienteRepository.deleteById(id);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("cliente/cadastro");
+
+        modelAndView.addObject("cliente", new Cliente());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public ModelAndView cadastrar(Cliente cliente) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/cliente");
+
+        clienteRepository.save(cliente);
 
         return modelAndView;
     }
